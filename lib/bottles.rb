54cb31d3e9aab1fb0a2931940eaacc3c0eb42296
Trish Rempel <trish.rempel@shopify.com>
2023-01-15 15:11:09 -0600

    Remove unnecessary duplicate methods from BottleNumber0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -106,14 +106,6 @@ class BottleNumber0 < BottleNumber
   def quantity
     "no more"
   end
-
-  def action
-    "Go to the store and buy some more"
-  end
-
-  def successor
-    BottleNumber.for(max, max: max, min: min)
-  end
 end
 
 class BottleNumber1 < BottleNumber
